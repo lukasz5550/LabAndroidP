c6eac61a3a641d2f216b19d1161d7ebc80d719a4
Student <Student>
2018-04-07 15:18:28 +0200

    Initial commit

---
+++ app/src/main/java/com/ldmitrowski/wizut/labandroidp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ldmitrowski.wizut.labandroidp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
